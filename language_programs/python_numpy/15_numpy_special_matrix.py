ed5722f24e8207fd7dd3f12101db1ad23dbcd6d3
Nirmal Vatsyayan <nirmal.vatsyayan1990@gmail.com>
2017-02-06 14:53:54 +0530

    numpy

---
+++ language_programs/python_numpy/15_numpy_special_matrix.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+if False:
+    #creating identity matrix
+    a = np.identity(4, dtype=float)
+    print(a)
+
+
+if True:
+	#creating eye matrix
+	#eye function creates matrices with ones along k-th diagonal
+    print("Ones at 1-st diagonal")
+    a = np.eye(4, k=1, dtype=float)
+    print(a)
+
+    print("Ones at 2-nd diagonal")
+    b = np.eye(4, k=2, dtype=float)
+    print(b)
+
+    print("Ones at negative 1-st diagonal")
+    b = np.eye(4, k=-1, dtype=float)
+    print(b)
